ae9638e47789c192a41f796dc80f0a6d0ca426bd
Soumyakanti Pan <soumyakanti8051@gmail.com>
2023-05-18 21:35:27 -0700

    finish 1.2

--- HW2/285hw2.R
+++ HW2/285hw2.R
@@ -2,6 +2,7 @@ rm(list = ls())
 
 library(lattice)
 library(latticeExtra)
+library(ggplot2)
 
 K.trunc = 20
 Sethu_jump = function(alpha, K = K.trunc){
@@ -28,10 +29,12 @@ generate_DPH = function(alpha, K, N.sample,
   }else stop("Incorrect base measure/parameters.")
 }
 
-N.S = 10
+set.seed(1729)
+N.S = 50
 sample.size = 100
+alphaseq = c(0.1, 0.5, 1, 10)
 dph.samples = NULL
-for(alpha in c(0.1, 0.5, 1, 10)){
+for(alpha in alphaseq){
   dph.samples.temp = array(dim = c(sample.size, N.S))
   for(i in 1:N.S){
     dph.samples.temp[, i] = generate_DPH(alpha = alpha, K = 20, 
@@ -44,9 +47,98 @@ for(alpha in c(0.1, 0.5, 1, 10)){
   dph.samples = rbind(dph.samples, dph.samples.temp)
 }
 alpha.labels = paste("\U03B1 =",
-                     c("0.1", "0.5", "1", "10"), sep = " ")
+                     as.character(alphaseq), sep = " ")
 dph.samples$alpha = rep(alpha.labels, each = sample.size)
 
 fmla1 = as.formula(paste("~ ", paste(xnam, collapse= "+"), " | alpha"))
 ecdfplot(fmla1, data = dph.samples,
          xlab = "", ylab = "", xaxt = "n", asp = 1.5)
+
+mv_functional = function(n.G, alpha, 
+                         sample.size = 100,
+                         K = 20, base = "norm",
+                         baseparams = c(0, 1)){
+  dph.samples.temp = array(dim = c(sample.size, n.G))
+  for(i in 1:n.G){
+    dph.samples.temp[, i] = generate_DPH(alpha = alpha, K = K, 
+                                         N.sample = sample.size,
+                                         base = base, 
+                                         baseparams = baseparams)
+  }
+  m1.G = apply(dph.samples.temp, 2, mean)                   # first moment
+  m2.G = apply(dph.samples.temp, 2, function(x) mean(x^2))  # second moment
+  v.G = m2.G - (m1.G)^2                                     # variance functional
+  return(cbind(m1.G, v.G))
+}
+
+n.G = 1000
+res = mv_functional(n.G = n.G, alpha = 0.1)
+hist(res[,1])
+
+mv.df = data.frame(mean = as.numeric(),
+                   var = as.numeric())
+for(alpha in alphaseq){
+  temp = mv_functional(n.G = n.G, alpha = alpha)
+  mv.df = rbind(mv.df, temp)
+}
+
+names(mv.df) = c("mean", "var")
+mv.df$alpha = rep(alpha.labels, each = n.G)
+
+plot.mu = ggplot(mv.df) +
+  geom_histogram(aes(x = mean, y = ..density..), 
+                 binwidth = 0.1) +
+  geom_density(aes(x = mean), lwd = 0.25, colour = 4,
+               fill = 4, alpha = 0.25) +
+  facet_wrap(~ alpha, ncol = 2) +
+  xlab(expression(mu(G))) +
+  theme_bw() +
+  theme(panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank())
+
+plot.var = ggplot(mv.df) +
+  geom_histogram(aes(x = var, y = ..density..), 
+                 binwidth = 0.1) +
+  facet_wrap(~ alpha, ncol = 2) +
+  xlab(expression(mu(G))) +
+  theme_bw() +
+  theme(panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank())
+
+gridExtra::grid.arrange(plot.mu, plot.var, ncol = 2)
+
+
+
+set.seed(1729)
+N.S = 50
+sample.size = 100
+alphaseq = c(0.1, 0.5, 1, 10)
+E.M.dph = array(dim = c(N.S, length(alphaseq)))
+for(j in 1:length(alphaseq)){
+  alpha = alphaseq[j]
+  dph.samples.temp = array(dim = c(sample.size, N.S))
+  for(i in 1:N.S){
+    dph.samples.temp[, i] = generate_DPH(alpha = alpha, K = 20, 
+                                         N.sample = sample.size,
+                                         base = "norm", baseparams = c(0, 1))
+  }
+  m = apply(dph.samples.temp, 2, function(x) length(unique(x)))
+  E.M.dph[, j] = m
+}
+E.M.theo = alphaseq * log((alphaseq + sample.size) / alphaseq)
+alpha.grid = c(seq(0, 0.5, length.out = 20),
+               seq(0.5, 1, length.out = 20),
+               seq(1, 10, length.out = 20))
+E.M.theo = alpha.grid * log((alpha.grid + sample.size) / alpha.grid)
+
+plot(alpha.grid, E.M.theo, col = "red", xlim = c(0, 10.5),
+     xlab = expression(paste(alpha)), ylab = expression(paste(E(M))), type = "l")
+boxplot(E.M.dph, boxwex = c(0.25, 0.25, 0.25, 0.75), xaxt = "n", 
+        whisklty = 1, whisklwd = 0.5, 
+        staplelty = 1, staplelwd = 0.5,
+        outpch = ".", outcol = "grey", outcex = 0.5, 
+        medcol = "darkblue", col = "lightblue",
+        at = alphaseq, add = T)
+
+
+
